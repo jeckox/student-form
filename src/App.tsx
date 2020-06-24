2dc629944f69ea9beaff63f8cfe61497c7561160
Gerardo Adrian Aburto Vigueras <gerardo.aburto@globant.com>
2020-06-24 00:06:44 -0500

    Added dependencies, configurated mocking library, created app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {makeServer} from './server';
 
-function App() {
+const App: React.FC= () => {
+  if (process.env.NODE_ENV === "development") {
+    makeServer()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+        <nav>
+          <Link to='/add-student'> Add Student </Link>
+          <Link to='/students'> All the students </Link>
+        </nav>
+        <Switch>
+            <Route path="/student/:studentId">
+            <p>Student</p>
+            </Route>
+            <Route path="/students">
+            <p>students</p>
+            </Route>
+            <Route path="/add-student">
+            <p>add student</p>
+            </Route>
+            <Route>
+              <p>Inicio</p>
+            </Route>
+      </Switch>
+    </Router>
+    );
+};
 
 export default App;
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import { Server, Model } from "miragejs";
+export function makeServer(): Server{
+    const server = new Server({
+        models:{
+            students:Model
+        },
+        routes(): void {
+            this.namespace = "/api";
+            this.get("/getStudents", schema => {
+                return schema.db.students;
+            });
+            this.post("/addStudents",(schema,request) => {
+                const theStudent = JSON.parse(request.requestBody);
+                return schema.db.student.insert(theStudent);
+            });
+            return;
+        },
+        seeds(server): void{
+            server.db.loadData({
+                levels:[
+                    { id:'0', firstName:'John', lastName: 'Doe', streetNumber:'1234 St. Name', city:'New York', phone:'+1 1234567890', gpa:'4.0', photoURL:'https://gravatar.com/avatar/4b74abd31f836d8a222665b22350bf30?s=400&d=robohash&r=x'},
+                ]
+              });
+            return;
+        }
+    });
+    return server;
+}
